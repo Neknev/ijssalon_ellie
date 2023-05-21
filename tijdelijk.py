b1a7c658e5568cab85d337118b28dc5abf874891
Karel Venken | HORECA VLAANDEREN <Karel Venken>
2023-05-21 17:23:49 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,30 +1,26 @@
-prijzen = {
-    "aardbei": 3,
-    "vanille": 4,
-    "chocolade": 5,
-}
-aanbieding = prijzen["aardbei"] * 0.8
+from helper import decoreer
 
-reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
-print(reclame_tekst)
+def print_aanbieding():
+    prijzen = {
+        "aardbei": 3,
+        "vanille": 4,
+        "chocolade": 5,
+    }
 
-reclame_tekst2 = reclame_tekst[:62]
-print(reclame_tekst2)
+    aanbieding = prijzen["aardbei"] * 0.8
 
-reclame_tekst3 = reclame_tekst2.upper()
-print(reclame_tekst3)
+    reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
 
-reclame_tekst4 = reclame_tekst3.split(" ")
-print(reclame_tekst4)
+    reclame_tekst2 = reclame_tekst[:63]
 
-for el in reclame_tekst4:
-    print(el)
+    reclame_tekst3 = reclame_tekst2.upper()
 
-for el in reclame_tekst4:
-    print(el.lower())
+    reclame_tekst4 = reclame_tekst3.split(" ")
 
-for el in reclame_tekst4:
-    if len(el) > 4: 
-        print(el.upper())
-    else:
-        print(el.lower())
+    for el in reclame_tekst4:
+        if len(el) > 4: 
+            print(el.upper())
+        else:
+            print(el.lower())
+print_aanbieding()
+decoreer("Aanbieding")
